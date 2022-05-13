8421c61df31415eb99fd27128de09e410c1b3fee
Sinziana Sovejanu <sv.sinziana@gmail.com>
2022-05-13 23:31:27 +0300

    first commit

---
+++ Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace distanceCar
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            var cons = int.Parse(Console.ReadLine());
+            var cap = int.Parse(Console.ReadLine());
+            int dist;
+            dist = cap * 100 / cons;
+            
+            Console.Write(dist);
+        }
+    }
+}
+
